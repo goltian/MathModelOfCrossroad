f2d3b204d27bc62ec915f46f7ee27d08c1cd9d8c
3817061LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2023-02-12 13:18:43 +0300

    add new method for calcualte best shape and rate. does not work too

--- work_with_RStudio/ChisqTestForGammaDist.R
+++ work_with_RStudio/ChisqTestForGammaDist.R
@@ -1,3 +1,5 @@
+library( MASS )
+
 ## Function for getting data (it's discrete) from experiment with our model
 get_data = function() {
   frequencies <- c(17, 110, 560, 1343, 2028, 2223, 1762, 1143, 541, 197, 58, 18)
@@ -20,8 +22,10 @@ frequencies
 parameters = fitdistr(data, "gamma")$estimate
 shape = parameters[1]
 rate = parameters[2]
+mean = mean(data)
 shape
 rate
+mean
 
 ## Get probabilities for the intervals
 rightVals = c(seq(13.25, 18.25, 0.5), Inf)
@@ -32,9 +36,43 @@ probs = pgamma(rightVals, shape, rate) - pgamma(leftVals, shape, rate)
 ## Chisq test does not work with sample shape and rate
 chisq.test(frequencies, p=probs)
 
+best_p = 0
+start_rate = 20
+rate_step = 0.001
+shape_step = 0.001
+for (rate_id in 1:1000) {
+  cur_rate = start_rate + rate_id * rate_step
+  
+  start_shape = 315
+  for (shape_id in 1:1000) {
+    cur_shape = start_shape + shape_id * shape_step
+    probs = (pgamma(rightVals, cur_shape, cur_rate) - 
+               pgamma(leftVals, cur_shape, cur_rate))
+    cur_res = chisq.test(frequencies, p=probs)
+    cur_p = cur_res$p.value
+    if (best_p < cur_p) {
+      best_res = cur_res
+      best_p = cur_p
+      best_rate = cur_rate
+      best_shape = cur_shape
+    }
+  }
+}
+best_res
+best_rate
+best_shape
+best_p
+
+probs = (pgamma(rightVals, best_shape, best_rate) 
+         - pgamma(leftVals, best_shape, best_rate))
+plot(x, probs, col="green", pch=4)
+points(x, frequencies / data_length, col="blue", pch=20)
+
+## With best shape and sample mean chisq test does not work
+chisq.test(frequencies, p=probs)
+
 ## Optimize p value and find best shape (sample mean = 15.4969)
 optimize_data_shape = function(cur_data_shape) {
-  mean = mean(data)
   cur_data_rate = cur_data_shape / mean
   probs = (pgamma(rightVals, cur_data_shape, cur_data_rate) - 
              pgamma(leftVals, cur_data_shape, cur_data_rate))
@@ -44,11 +82,13 @@ optimize_data_shape = function(cur_data_shape) {
 
 optimize_result = optimize(optimize_data_shape, interval = c(300, 320),
                            maximum = TRUE, tol = 0.0001)
-best_shape = optimize_result$maximum
-best_shape
-best_rate = best_shape / mean(data)
+optimize_shape = optimize_result$maximum
+optimize_shape
+optimize_rate = optimize_shape / mean
+optimize_rate
 
-probs = pgamma(rightVals, best_shape, best_rate) - pgamma(leftVals, best_shape, best_rate)
+probs = (pgamma(rightVals, optimize_shape, optimize_rate) 
+         - pgamma(leftVals, optimize_shape, optimize_rate))
 plot(x, probs, col="green", pch=4)
 points(x, frequencies / data_length, col="blue", pch=20)
 
