8c859ad776fa62697237de47dc6731d35fafea9a
3817061LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2023-03-09 17:03:41 +0300

    50 000 exps for gamma dist with puasson does not work

--- work_with_RStudio/ChisqTestForGammaDist.R
+++ work_with_RStudio/ChisqTestForGammaDist.R
@@ -61,6 +61,70 @@ points(x, frequencies / data_length, col="blue", pch=20)
 chisq.test(frequencies, p=probs)
 
 ##########################################################################
+## Same tests for 50 000 exps
+
+## Function for getting data (it's discrete) from experiment with our model
+get_data = function() {
+  frequencies <- c(71, 571, 2772, 6667, 10109, 11070, 8947, 5688, 2760, 1016, 255, 74)
+  values <- seq(13, 18.5, 0.5)
+  data <- rep(values, frequencies)
+  return (data)
+}
+
+## Get data
+data = get_data()
+data_length = length(data)
+
+table = table(data)
+values = as.numeric(names(table))
+frequencies = as.numeric(table)
+values
+frequencies
+
+## Compute shape and rate
+parameters = fitdistr(data, "gamma")$estimate
+shape = parameters[1]
+rate = parameters[2]
+mean = mean(data)
+shape
+rate
+mean
+
+## Get probabilities for the intervals
+rightVals = c(seq(13.25, 18.25, 0.5), Inf)
+leftVals = c(-Inf, seq(13.25, 18.25, 0.5))
+x = seq(13, 18.5, 0.5)
+probs = pgamma(rightVals, shape, rate) - pgamma(leftVals, shape, rate)
+
+## Chisq test does not work with sample shape and rate
+chisq.test(frequencies, p=probs)
+
+## Optimize p value and find best shape (sample mean = 15.4969)
+optimize_data_shape = function(cur_data_shape) {
+  cur_data_rate = cur_data_shape / mean
+  probs = (pgamma(rightVals, cur_data_shape, cur_data_rate) - 
+             pgamma(leftVals, cur_data_shape, cur_data_rate))
+  cur_res = chisq.test(frequencies, p=probs)
+  return (cur_res$p.value)
+}
+
+optimize_result = optimize(optimize_data_shape, interval = c(300, 320),
+                           maximum = TRUE, tol = 0.0001)
+optimize_shape = optimize_result$maximum
+optimize_shape
+optimize_rate = optimize_shape / mean
+optimize_rate
+
+probs = (pgamma(rightVals, optimize_shape, optimize_rate) 
+         - pgamma(leftVals, optimize_shape, optimize_rate))
+plot(x, probs, col="green", pch=4)
+points(x, frequencies / data_length, col="blue", pch=20)
+
+## With best shape and sample mean chisq test does not work
+chisq.test(frequencies, p=probs)
+
+
+##########################################################################
 
 ## Same tests for Bartlett P1 and P2 flows
 ## Function for getting data (it's discrete) from experiment with our model
